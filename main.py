43063d20bfaa83151eb9607561b028b4f7337588
Jonathan Behrens <fintelia@gmail.com>
2025-01-16 20:54:13 -0800

    Adjust model for 4+2 rezoning

--- main.py
+++ main.py
@@ -5,73 +5,101 @@ import matplotlib.pyplot as plt
 import math
 from os.path import exists
 
-lot_area_per_dus ={
-    #'A-1': 6000,
-    #'A-2': 4500,
-    #'B': 2500,
-    'C': 1800,
-    'C-1': 1500,
-    'C-1A': 1000,
-    'C-2': 600,
-    'C-2A': 300,
-    'C-2B': 300,
-    'C-3': 300,
-    'C-3A': 300,
-    'C-3B': 300,
+# lot_area_per_dus ={
+#     #'A-1': 6000,
+#     #'A-2': 4500,
+#     #'B': 2500,
+#     'C': 1800,
+#     'C-1': 1500,
+#     'C-1A': 1000,
+#     'C-2': 600,
+#     'C-2A': 300,
+#     'C-2B': 300,
+#     'C-3': 300,
+#     'C-3A': 300,
+#     'C-3B': 300,
 
-    # 'BA': 600,
-    # 'BA-1': 1200,
-    # 'BA-2': 600,
-    # 'BA-3': 1500,
-    # 'BA-4': 600,
-    'BB': 300,
-    # 'BB-1': 300,
-    # 'BB-2': 300,
-    # 'BC': 500,
-    # 'BC-1': 450,
+#     # 'BA': 600,
+#     # 'BA-1': 1200,
+#     # 'BA-2': 600,
+#     # 'BA-3': 1500,
+#     # 'BA-4': 600,
+#     'BB': 300,
+#     # 'BB-1': 300,
+#     # 'BB-2': 300,
+#     # 'BC': 500,
+#     # 'BC-1': 450,
 
-    # 'O-1': 1200,
-    # 'O-2': 600,
-    # 'O-2A': 600,
-    # 'O-3': 300,
-    # 'O-3A': 300,
+#     # 'O-1': 1200,
+#     # 'O-2': 600,
+#     # 'O-2A': 600,
+#     # 'O-3': 300,
+#     # 'O-3A': 300,
 
-    # 'IA-1': 700,
-    # # 'IA-2': 1,
+#     # 'IA-1': 700,
+#     # # 'IA-2': 1,
+# }
+open_space = {
+    'A-1': 0.3,
+    'A-2': 0.3,
+    'B': 0.3,
+    'C': 0.3,
+    'C-1': 0.3,
+    'C-1A': 0.15,
+    'C-2': 0.15,
+    'C-2A': 0.1,
+    'C-2B': 0.15,
+    'C-3': 0.1,
+    'C-3A': 0.1,
+    'C-3B': 0.1,
+
+    'BA': 0,
+    'BA-1': 0,
+    'BA-2': 0,
+    'BA-3': 0.3,
+    'BA-4': 0,
+    'BB': 0,
+    'BB-1': .3,
+    'BB-2': .3,
+    'BC': 0,
+
+    'O-1': 0.15,
+    'O-2': 0.15,
+    'O-2A': 0.15,
+    'O-3': 0.1,
+    'O-3A': 0.1,
+
+    'IA-1': 0,
+    'IA-2': 0,
 }
-floor_area_ratios = {
-    # 'A-1': 0.5,
-    # 'A-2': 0.5,
-    # 'B': 0.5,
-    'C': 0.6,
-    'C-1': 0.75,
-    'C-1A': 1.25,
-    'C-2': 1.75,
-    'C-2A': 2.5,
-    'C-2B': 1.75,
-    'C-3': 3,
-    'C-3A': 3,
-    'C-3B': 4,
 
-    'BA': 1.75,
-    'BA-1': 0.75,
-    'BA-2': 1.75,
-    'BA-3': 0.75,
-    'BA-4': 1.75,
-    'BB': 3,
-    'BB-1': 3.25,
-    'BB-2': 3,
-    'BC': 2,
-    'BC-1': 3,
+stories = {
+    'C-1A': 6,
+    'C-2': 7,
+    'C-2A': 6,
+    'C-2B': 6,
+    'C-3': 10,
+    'C-3A': 10,
+    'C-3B': 10,
 
-    'O-1': 0.75,
-    'O-2': 2,
-    'O-2A': 1.5,
-    'O-3': 3,
-    'O-3A': 3,
+    'BA': 6,
+    'BA-1': 6,
+    'BA-2': 6,
+    'BA-3': 6,
+    'BA-4': 6,
+    'BB': 7,
+    'BB-1': 8,
+    'BB-2': 6,
+    'BC': 6,
 
-    'IA-1': 1.5,
-    # 'IA-2': 4,
+    'O-1': 6,
+    'O-2': 7,
+    'O-2A': 6,
+    'O-3': 10,
+    'O-3A': 10,
+
+    'IA-1': 6,
+    'IA-2': 6,
 }
 
 pandas.set_option("display.precision", 2)
@@ -91,7 +119,7 @@ density_deduct = density_deduct.overlay(boundary).dissolve()
 parcels = geopandas.read_file("49_CAMBRIDGE_basic.zip").to_crs("EPSG:2249")
 parcels.geometry = parcels.buffer(-1)
 zones = geopandas.read_file("CDD_ZoningDistricts.shp.zip").to_crs("EPSG:2249")
-zones = zones[zones.area > 5 * 43560]
+#zones = zones[zones.area > 5 * 43560]
 
 assessing_parcels = geopandas.read_file("ASSESSING_ParcelsFY2023.shp.zip").to_crs("EPSG:2249")
 assessing_parcels.set_index("LOC_ID", inplace=True, verify_integrity=True)
@@ -103,30 +131,33 @@ properties['existing_units'] = properties.apply(lambda row: 1 if row['PropertyCl
 properties = properties[['GISID', 'existing_units']].groupby('GISID').sum()
 parcels = parcels.join(properties, on="ML", how="left", rsuffix="_properties")
 
-print(zones['ZONE_TYPE'].unique())
-
 def units_in_zone(zone):
     zone_type = zone['ZONE_TYPE']
     district_parcels = parcels[parcels.geometry.within(zone.geometry)]
     existing_units = district_parcels['existing_units'].sum()
-    if zone_type not in lot_area_per_dus:
+    if zone_type == "OS":
         return [0, 0, district_parcels['existing_units'].sum()]
 
-    # District parameters
-    lot_area_per_du = lot_area_per_dus[zone_type]
-    floor_area_ratio = floor_area_ratios[zone_type]
-    unit_cap = 11
-    if zone_type in ["BA", "BA-4", "BB", "BB-1", "BB-2", "BC", "IA-1", "C-2", "C-2A", "C-2B", "C-3", "C-3A", "C-3B"]:
-        unit_cap = 100000
 
     # Calculate units
     total_units = 0
     station_units = 0
     for _, parcel in district_parcels.iterrows():
         sqft = parcel['SQFT'] - parcel['Tot_Exclud']
-        units = min(math.floor(sqft / lot_area_per_du), round(sqft * floor_area_ratio / 1000), unit_cap)
-        if units < 3:
+
+        height = 4
+        if sqft >= 5000:
+            height = 6
+        height = stories.get(zone_type, height)
+
+        max_coverage = 1 - max(open_space.get(zone_type, 0.3), .2)
+        floor_area = min(sqft * max_coverage * height, 75000)
+
+        if floor_area < 2500:
             continue
+
+        units = max(math.floor(floor_area / 1000), 3)
+
         # if units <= parcel['existing_units']:
         #     continue
         # if parcel['existing_units'] > 20:
@@ -143,7 +174,7 @@ def units_in_zone(zone):
     return [total_units, station_units, existing_units]
 
 zones[['units', 'station_units', 'existing_units']] = zones.apply(units_in_zone, axis=1, result_type='expand')
-zones['density_denominator'] = zones['geometry'].difference(density_deduct['geometry'].unary_union).area / 43560
+zones['density_denominator'] = zones['geometry'].difference(density_deduct['geometry'].union_all()).area / 43560
 zones['density'] = zones['units'] / zones['density_denominator']
 
 print("Total possible units:", zones['units'].sum())
@@ -180,7 +211,7 @@ else:
     print("[FAIL] Total units: %d" % total_units)
 
 try:
-    station_area_acres = district['geometry'].unary_union.intersection(station_area.geometry.unary_union).area / 43560
+    station_area_acres = district['geometry'].union_all().intersection(station_area.geometry.union_all()).area / 43560
 except:
     station_area_acres = 0
 if station_area_acres >= 32 * 0.9:
@@ -200,7 +231,7 @@ if total_density >= 15:
 else:
     print("[FAIL] Density: %.2f units/acre" % total_density)
 
-district_parts = geopandas.GeoSeries(district['geometry'].unary_union).explode(index_parts=False)
+district_parts = geopandas.GeoSeries(district['geometry'].union_all()).explode(index_parts=False)
 contiguous_fraction = district_parts.area.max() / district_parts.area.sum()
 if contiguous_fraction >= 0.5:
     print("[PASS] Contiguous fraction: %.2f%%" % (contiguous_fraction * 100))
@@ -208,7 +239,7 @@ else:
     print("[FAIL] Contiguous fraction: %.2f%%" % (contiguous_fraction * 100))
 
 # Plot the district
-origin = zones.unary_union.centroid
+origin = zones.union_all().centroid
 ax = boundary.rotate(15, origin=origin).plot(figsize=(16,12), color='white', edgecolor='black', linewidth=2)
 district.where(district['ZONE_TYPE'] != 'BB').rotate(15, origin=origin).plot(ax=ax, color='lightblue', edgecolor='black')
 district.where(district['ZONE_TYPE'] == 'BB').rotate(15, origin=origin).plot(ax=ax, color='turquoise', edgecolor='black')
